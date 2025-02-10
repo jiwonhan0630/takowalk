342f24439e0ccf37cefcb7016a2f2805833faa73
한지원 <jiwonhan0630@gmail.com>
2025-02-10 12:47:18 +0900

    리스트에 등록하는 함수 추가

--- takowalk/Assets/Scripts/GameSystemUpdater.cs
+++ takowalk/Assets/Scripts/GameSystemUpdater.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,6 +10,28 @@ namespace Takowalk
     {
         private List<GameSystemBase> _updateList = new();
         private List<GameSystemBase> _fixedUpdateList = new();
+
+        public void RegisterUpdate(GameSystemBase gameSystem)
+        {
+            var old = _updateList.FindIndex((item) => gameSystem.GetType() == item.GetType());
+            if (old >= 0)
+            {
+                _updateList.RemoveAt(old);
+            }
+
+            _updateList.Add(gameSystem);
+        }
+
+        public void RegisterFixedUpdate(GameSystemBase gameSystem)
+        {
+            var old = _fixedUpdateList.FindIndex((item) => gameSystem.GetType() == item.GetType());
+            if (old >= 0)
+            {
+                _fixedUpdateList.RemoveAt(old);
+            }
+
+            _fixedUpdateList.Add(gameSystem);
+        }
 
         private void Update()
         {
